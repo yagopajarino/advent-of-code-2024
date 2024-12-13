9a4eeec0a01a131dd4426bc1a3bcde0411accffa
Yago Pajariño <yagopajarino@outlook.com>
2024-12-13 16:59:48 -0300

    Modify problem4 solution

--- problem_4/src/main.rs
+++ problem_4/src/main.rs
@@ -6,4 +6,13 @@ fn main() {
     let file_path = &args[1];
 
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    let lines: Vec<&str> = contents.lines().collect();
+    let mut matrix = vec![vec![""; lines.len()]; lines.len()];
+    let mut fila = 0;
+    for line in lines {
+        line.split("").enumerate().for_each(|(col, value)| {
+            matrix[fila][col] = value;
+        });
+        fila += 1;
+    }
 }
